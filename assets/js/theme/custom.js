24881f9ee2a80ba16d16254dcc5381c6e543b39c
Aditya Chauhan <adie@nuethic.com>
2019-07-15 10:55:33 -0600

    handing code over to Kaidub

--- assets/js/theme/custom.js
+++ assets/js/theme/custom.js
@@ -1,6 +1,7 @@
 function hideSearch() {
     const x = document.querySelectorAll('.quickSearchResults');
-    for (let i = 0; i < x.length; i++){
+    for (let i = 0; i < x.length; i++) {
         x[i].style.display = 'none';
     }
 }
+console.log(hideSearch());
